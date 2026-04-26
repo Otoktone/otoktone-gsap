b93759895c61ccc334469f97ca1ad69a82f44411
Otoktone <alexandre.desmot@gmail.com>
2026-04-26 12:52:13 +0200

    front: add hyperspeed component

--- src/app/components/Sections/Hero/Hero.tsx
+++ src/app/components/Sections/Hero/Hero.tsx
@@ -2,18 +2,17 @@
 
 import { useEffect, useRef } from 'react';
 import { fadeIn } from '@/app/utils/animation';
+import Hyperspeed from './Hyperspeed';
 import styles from './Hero.module.scss';
 
 const Hero = () => {
     const subTitle = 'Otoktone';
     const title = "Conception et développement d'applications web";
 
-    const backgroundRef = useRef<HTMLDivElement>(null);
     const h1Ref = useRef<HTMLHeadingElement>(null);
     const h2Ref = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        fadeIn(backgroundRef.current, 0, 0.2, 3);
         fadeIn(h1Ref.current, 0, 0.65, 3);
         fadeIn(h2Ref.current, 0, 0.7, 3);
     }, []);
@@ -21,10 +20,43 @@ const Hero = () => {
     return (
         <section id={styles.hero}>
             <div className={styles.heroHeaderContainer}>
-                <div
-                    ref={backgroundRef}
-                    className={styles.heroHeaderBackground}
-                ></div>
+                <Hyperspeed
+                    effectOptions={{
+                        distortion: 'turbulentDistortion',
+                        length: 400,
+                        roadWidth: 10,
+                        islandWidth: 2,
+                        lanesPerRoad: 4,
+                        fov: 90,
+                        fovSpeedUp: 150,
+                        speedUp: 2,
+                        carLightsFade: 0.4,
+                        totalSideLightSticks: 20,
+                        lightPairsPerRoadWay: 40,
+                        shoulderLinesWidthPercentage: 0.05,
+                        brokenLinesWidthPercentage: 0.1,
+                        brokenLinesLengthPercentage: 0.5,
+                        lightStickWidth: [0.12, 0.5],
+                        lightStickHeight: [1.3, 1.7],
+                        movingAwaySpeed: [60, 80],
+                        movingCloserSpeed: [-120, -160],
+                        carLightsLength: [400 * 0.03, 400 * 0.2],
+                        carLightsRadius: [0.05, 0.14],
+                        carWidthPercentage: [0.3, 0.5],
+                        carShiftX: [-0.8, 0.8],
+                        carFloorSeparation: [0, 5],
+                        colors: {
+                            roadColor: 0x080808,
+                            islandColor: 0x0a0a0a,
+                            background: 0x000000,
+                            shoulderLines: 0xffffff,
+                            brokenLines: 0xffffff,
+                            leftCars: [0xd856bf, 0x6750a2, 0xc247ac],
+                            rightCars: [0x03b3c3, 0x0e5ea5, 0x324555],
+                            sticks: 0x03b3c3,
+                        },
+                    }}
+                />
                 <div className={styles.heroHeaderTitle}>
                     <h1 ref={h1Ref}>{title}</h1>
                     <h2 ref={h2Ref}>{subTitle}</h2>
